0d9b97aa4637837139f2ca3fe6a7bffe0c54a9d7
Enggar Tivandi <64598048+nekoding@users.noreply.github.com>
2020-11-08 15:26:18 +0800

    add preloader

--- src/components/_loadListTeams.js
+++ src/components/_loadListTeams.js
@@ -2,8 +2,10 @@ import { requestData } from './_api'
 import { createList } from './_createList'
 
 const loadListTeams = () => {
+  const preloader = document.querySelector('#preloader')
   requestData('competitions/2021/teams')
     .then(data => {
+      preloader.style.display = 'none'
       const content = document.querySelector('#main-content')
       data.teams.forEach(team => {
         content.innerHTML += createList(team)
--- src/teams.js
+++ src/teams.js
@@ -8,17 +8,23 @@ const urlSearchParams = new URLSearchParams(window.location.search)
 const idTeams = urlSearchParams.get('id')
 const saveButton = document.querySelector('#save')
 const isFromFavorite = (window.location.hash.substr(1) === 'saved')
+const preloader = document.querySelector('#preloader')
 
 if (isFromFavorite) {
   saveButton.style.display = 'none'
+  preloader.style.display = 'none'
   loadSavedTeamInfo(idTeams)
 } else {
   loadTeamInfo(idTeams)
+    .then(() => {
+      preloader.style.display = 'none'
+    })
 }
 
 saveButton.onclick = () => {
   loadTeamInfo(idTeams)
     .then(data => {
+      preloader.style.display = 'none'
       saved(data)
     })
     .catch(err => console.error(err))
